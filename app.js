1fb010928f7780b40734bf8a7aaa7d8ba3fe9a30
waadshayea8@gmail.com <waadshayea8@gmail.com>
2026-08-17 13:24:40 +0300

    Check clicked object

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 let objects = document.querySelectorAll(".object");
 
-for (let i =0; i<objects.length; i++){
-    objects[i].addEventListener("click",function(){
-    
+for (let i = 0; i < objects.length; i++) {
+    objects[i].addEventListener("click", function () {
+
         let clickedObject = objects[i].id;
-        if (clickedObject === wantedObject){
+        if (clickedObject === wantedObject) {
             console.log("correct!");
         }
-    
-});
+
+    });
 }
 
 
@@ -18,63 +18,96 @@ let message = document.getElementById("message");
 
 let wantedObject;
 
-function chooseObject(){
+function chooseObject() {
 
-let randomnumber= Math.floor(Math.random()* objects.length);
-wantedObject=objects[randomnumber].id;
+    let randomnumber = Math.floor(Math.random() * objects.length);
+    wantedObject = objects[randomnumber].id;
 
 }
 
 chooseObject();
-message.textContent= "zozi wants " + wantedObject + "!";
+message.textContent = "zozi wants " + wantedObject + "!";
 
 
 
 let timerText = document.getElementById("timer");
- 
+
 let timer;
 
 let timerLeft = 20;
 
 let gameStarted = false;
 
-function startTimer(){
+function startTimer() {
 
     timeLeft = 20;
 
-    timerText.textContent = "Time: "+ timerLeft;
+    timerText.textContent = "Time: " + timerLeft;
 
-timer = setInterval(function(){
+    timer = setInterval(function () {
 
-    timeLeft--;
+        timeLeft--;
 
-    timer.textContent = "Time: "+ timerLeft;
+        timer.textContent = "Time: " + timerLeft;
 
-    if(timerLeft === 0) {
+        if (timerLeft === 0) {
 
-        clearInterval(timer);
+            clearInterval(timer);
 
-        gameStarted = false;
+            gameStarted = false;
 
-        message.textContent = "You lose!";
-    }
-},1000);
+            message.textContent = "You lose!";
+        }
+    }, 1000);
 }
 
 
 let playAgain = document.getElementById("startGame");
 
-startGame.addEventListener("click", function(){
+startGame.addEventListener("click", function () {
     gameStarted = true;
     chooseObject();
-    startTimer;
+    startTimer();
 });
 
 
 
+for (let i = 0; i < objects.length; i++) {
+
+    let object = objects[i];
+
+    object.addEventListener("click", function () {
+
+        if (gameStarted === false) {
+            return;
+        }
+
+        let clickedObject = object.id;
+
+        if (clickedObject === wantedObject) {
+
+            clearInterval(timer);
+
+            gameStarted = false;
+
+            message.textContent = "You Win!";
 
 
- 
+        } else {
+
+            message.textContent = "Wrong! Try again!";
+
+        }
+    });
+}
+
+
+
+
+
+
+
+
 
 
 
