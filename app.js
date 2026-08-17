2502e8bc05e18397ec36c84e9f20c0a2a3c28921
waadshayea8@gmail.com <waadshayea8@gmail.com>
2026-08-17 06:43:05 +0300

    Added the random request message

--- app.js
+++ app.js
@@ -8,3 +8,32 @@ for (let i =0; i<objects.length; i++){
 
 
 
+
+
+let message = document.getElementById("message");
+
+let wantedObject = "cookies";
+
+function chooseObject(){
+
+let randomnumber= Math.floor(Math.random()* objects.length);
+wantedObject=objects[randomnumber].id;
+
+}
+message.textContent= "zozi wants " + wantedObject + "!";
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
